a3cbd06d15c94694bc3e8452d4978f99aa735af2
morph1904 <morph1904@users.noreply.github.com>
2018-05-08 21:55:01 +0100

    Dev (#4)
    * Added insecure_skip_verify as default on all proxy hosts. Bug found thanks to itsmesid on Reddit.
    
    * fixed an issue with automatic reload of Caddy.
    fixed an issue forcing HTTPS when TLS is not selected.

--- TygerCaddy/hosts/caddyfile.py
+++ TygerCaddy/hosts/caddyfile.py
@@ -9,10 +9,7 @@ from django.contrib.auth.models import User
 def generate_caddyfile():
     project = settings.BASE_DIR
     caddyfilepath = project + '/caddyfile.conf'
-    # os.remove(caddyfilepath)
-    print(caddyfilepath)
-    print(os.remove(caddyfilepath))
-    print('Caddyfile deleted')
+
     caddyfile = open(caddyfilepath, "w+")
 
     user = User.objects.get(pk=1)
@@ -20,26 +17,25 @@ def generate_caddyfile():
     config = Config.objects.get(pk=1)
 
     for caddyhost in host_set:
-        print('Generating ' + caddyhost.host_name)
-        domain = caddyhost.host_name + ' { \n \n'
-        print('Proxy ' + caddyhost.proxy_host)
+        if (caddyhost.tls == False):
+            domain = caddyhost.host_name + ':80 { \n \n'
+        else:
+            domain = caddyhost.host_name + ' { \n \n'
+
         proxy = 'proxy / ' + caddyhost.proxy_host + ' { \n' \
                 'transparent \n' \
                 'insecure_skip_verify' \
                 '  } \n'
 
         if caddyhost.tls:
-            print('TLS ' + user.email)
             caddytls = 'tls ' + user.email + '\n } \n \n'
             caddyfile.write(domain + proxy + caddytls)
-
         else:
-            print('No TLS ')
             proxy = proxy + '} \n \n'
             caddyfile.write(domain + proxy)
+            caddyfile.close()
 
     caddyfile = open(caddyfilepath, "a")
-    print('Generating default host' + config.interface)
     domain = config.interface + ' { \n \n'
 
     proxy = 'proxy / ' + config.proxy_host + ' { \n' \
@@ -49,16 +45,16 @@ def generate_caddyfile():
     root = 'root /apps/TygerCaddy/TygerCaddy/ \n' \
            '} \n'
     caddyfile.write(domain + proxy + root)
-    print('Finished')
+    caddyfile.close()
 
-    with open(project + '/caddypid.txt', 'r') as caddyservice:
-        caddypid = caddyservice.read().replace('\n', '')
-        print(caddypid)
+    # with open(project + '/caddypid.txt', 'r') as caddyservice:
+    #   caddypid = caddyservice.read().replace('\n', '')
+    #  print(caddypid)
 
-    command = "kill -s USR1 " + caddypid
-    print(command)
-    reload = subprocess.run(command, shell=True)
-    print(reload)
+    # command = "kill -s USR1 " + caddypid
+    # print(command)
+    # reload = subprocess.run(command, shell=True)
+    subprocess.call(['sudo', 'service', 'caddy', 'reload'])
     return True
 
 
