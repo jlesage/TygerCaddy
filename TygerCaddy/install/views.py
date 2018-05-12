b3acb7e0b296645eab0aebf2bb2195edb27d6624
morph1904 <morph1904@gmail.com>
2018-05-12 23:17:03 +0100

    Major Update - Created GUI installer - Moved CaddyFile and DB location - Created installer middleware - Created secret key generator - Updated install script - Amended Caddy Service - New Assets - Reduced dependencies
    To Do
    - Enable dash colour picker
    - Form Validation on installer
    - fix layout issue in installer
    - fix checkbox issue in installer
    - fix appearance of DNS provider dropdown and selection based on checkbox status
    
    Signed-off-by: morph1904 <morph1904@gmail.com>

---
+++ TygerCaddy/install/views.py
@@ -0,0 +1,66 @@
+import random
+import string
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from hosts.caddyfile import *
+from hosts.models import Config
+
+
+# Create your views here.
+
+
+class Index(View):
+    def get(self, request):
+        return render(request, 'install/install.html')
+
+    def post(self, request):
+        call_command('migrate')
+        call_command('loaddata', 'dns')
+        call_command('loaddata', 'variables')
+        form = request.POST
+        dnsbool = 0
+        if form['dns'] == 'on':
+            dnsbool = '1'
+        config = {
+            'username': form['username'],
+            'password': form['password'],
+            'email': form['email'],
+            'interface': form['interface'],
+            'port': form['port'],
+            'proxy-host': form['proxy-host'],
+            'dns': dnsbool,
+            'dns-provider': form['dns_provider'],
+            'dash-colour': form['dash-colour']
+        }
+
+        admin = User.objects.create_superuser(config['username'], config['email'], config['password'])
+        new_config = Config(interface=config['interface'],
+                            proxy_host=config['proxy-host'],
+                            name='primary', port=config['port'],
+                            proxy_exception='/assets',
+                            root_dir='/apps/TygerCaddy/TygerCaddy',
+                            dns_challenge=dnsbool,
+                            dns_provider_id=config['dns-provider'])
+        new_config.save()
+        generate_keyfile()
+        generate_dash()
+
+        return redirect('login')
+
+
+def generate_keyfile():
+    chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"',
+                                                                                                         '').replace(
+        '\\', '')
+
+    SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
+    keyfile = settings.BASE_DIR + '/data/secret.txt'
+
+    key = open(keyfile, 'w+')
+    key.write(SECRET_KEY)
+    key.close()
+    return True
