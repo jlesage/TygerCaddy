9669d18c1408be601197a30a39e7ffbb4968dfff
Sparky <sparky@possumlodge.me>
2018-05-12 08:52:26 -0400

    Container not ready for normal use! NO PERSISTENCE!
    Added docker-compose with example syntax
    Added README.md with example syntax
    Finalized Dockerfile and entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,9 +9,9 @@ python3 $TYGER_DIR/manage.py loaddata variables
 
 script="
 from django.contrib.auth.models import User;
-username = 'admin';
-password = 'secret';
-email = 'admin@example.com';
+username = '$username';
+password = '$password';
+email = '$email';
 if User.objects.filter(username=username).count()==0:
     User.objects.create_superuser(username, email, password);
     print('Superuser created.');
