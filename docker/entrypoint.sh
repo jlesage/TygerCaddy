71431735528bcf0ad1adfb11830a4b006d8d6635
morph1904 <morph1904@users.noreply.github.com>
2018-05-12 14:05:31 +0100

    Release! (#45)
    * Design fixes and changes (#38)
    
    * Design fixes
    Included few missing files.
    Removed document.write on html
    
    * Login design changes
    
    Signed-off-by: Siddhu <arevindh@gmail.com>
    
    * Design changes for delete host (#41)
    
    * Design fixes
    Included few missing files.
    Removed document.write on html
    
    * Login design changes
    
    Signed-off-by: Siddhu <arevindh@gmail.com>
    
    * Design fixes and changes (#38) (#39)
    
    * Design fixes
    Included few missing files.
    Removed document.write on html
    
    * Login design changes
    
    Signed-off-by: Siddhu <arevindh@gmail.com>
    
    * Initial support for Docker (#42)
    
    * Started dockerfile. Docker now builds but server is non-functional
    
    * Docker now runs. Need to fix the user account problem next
    
    * Default user created. Still need to allow specifying user account details
    
    * Changed git clone to my repo temporarily
    
    * Another try
    
    * Container not ready for normal use! NO PERSISTENCE!
    
    Added docker-compose with example syntax
    Added README.md with example syntax
    Finalized Dockerfile and entrypoint.sh

---
+++ docker/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TYGER_DIR=/apps/TygerCaddy/TygerCaddy
+
+python3 $TYGER_DIR/manage.py migrate
+python3 $TYGER_DIR/manage.py loaddata config
+python3 $TYGER_DIR/manage.py loaddata dns
+python3 $TYGER_DIR/manage.py loaddata variables
+
+script="
+from django.contrib.auth.models import User;
+username = '$username';
+password = '$password';
+email = '$email';
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python3 $TYGER_DIR/manage.py shell
+
+/usr/local/bin/uwsgi --emperor /apps/TygerCaddy/uwsgi.ini &
+/usr/local/bin/caddy -log stdout -pidfile=$TYGER_DIR/caddypid.txt -agree=true -conf=$TYGER_DIR/caddyfile.conf -root=/var/tmp
