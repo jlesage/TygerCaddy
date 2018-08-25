f47c67593d7652962343110d021ef47c2768fce4
morph1904 <morph1904@users.noreply.github.com>
2018-08-25 19:24:52 +0100

    Added utility API endpoints
    Signed-off-by: morph1904 <morph1904@gmail.com>

--- TygerCaddy/TygerCaddy/api_urls.py
+++ TygerCaddy/TygerCaddy/api_urls.py
@@ -1,4 +1,4 @@
-from rest_framework import routers
+from config.api.api_views import ConfigViewset
 from hosts.api.api_views import HostViewset
 from proxies.api.api_views import ProxyViewset, HeaderViewset
 from rest_framework import routers
@@ -8,3 +8,4 @@ router.register(r'hosts', HostViewset)
 router.register(r'proxies', ProxyViewset)
 router.register(r'headers', HeaderViewset)
 router.register(r'config', ConfigViewset)
+
